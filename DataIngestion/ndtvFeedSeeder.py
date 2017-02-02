158d74805d616999fd63779d1b0f1f16f1e97c7b
c-shekhar <chandrashekhar.kandpal@innovaccer.com>
2017-02-02 13:13:10 +0530

    updated feedSeeder for testing purposes

--- DataIngestion/ndtvFeedSeeder.py
+++ DataIngestion/ndtvFeedSeeder.py
@@ -12,9 +12,10 @@ class ndtvFeeds():
 		self.crawledFeeds = self.db[self.feedsCollectionName].distinct("feedLink")
 	
 	def getUrlsFromFeed(self, feedLink):
-		if feedToCrawl not in self.crawledFeeds:
+		if feedLink not in self.crawledFeeds:
 			parsedFeed = feedparser.parse(feedLink)
-			print parsedFeed['entries']
+			for eachFeed in parsedFeed['entries']:
+				print eachFeed,"\n"
 
 	def crawlUrl(self, urlToCrawl):
 		pass
--- ingestionController.py
+++ ingestionController.py
@@ -2,4 +2,5 @@ from DataIngestion.ndtvFeedSeeder import ndtvFeedsObject
 from ingestionConfig import *
 import sys
 feedToCrawl = sys.argv[1]
+# feedToCrawl = "http://feeds.feedburner.com/ndtvmovies-latest"
 ndtvFeedsObject.getUrlsFromFeed(feedToCrawl)
