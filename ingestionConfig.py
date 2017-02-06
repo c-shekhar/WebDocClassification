1b52f3d1b2cc4e5849e52f9a958df33ed1b8b5ba
gmayank32 <gmayank32@gmail.com>
2017-02-07 01:35:29 +0530

    changed constants to follow uppercase and underscore

--- ingestionConfig.py
+++ ingestionConfig.py
@@ -1,9 +1,9 @@
 from pymongo import MongoClient
 
-hostName = "localhost"
-portName = 27017
-clientObject = MongoClient(hostName,portName)
-dbName = "webDocClassification"
-dbObject = clientObject[dbName]
-feedsCollectionName = "Feeds"
-dataCollectionName = "Data"
+HOST_NAME = "localhost"
+PORT_NAME = 27017
+CLIENT_OBJECT = MongoClient(HOST_NAME,PORT_NAME)
+DATABASE_NAME = "webDocClassification"
+db = CLIENT_OBJECT[DATABASE_NAME]
+FEEDS_COLLECTION_NAME = "feedsInfo"
+DATA_COLLECTION_NAME = "htmlInfo"
