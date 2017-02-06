2ff5346dc1377fc606a3065cbdee4be1d7938350
gmayank32 <gmayank32@gmail.com>
2017-02-07 01:37:02 +0530

    changed the called function

--- ingestionController.py
+++ ingestionController.py
@@ -1,6 +1,7 @@
-from DataIngestion.ndtvFeedSeeder import ndtvFeedsObject
+from DataIngestion.Feeds import *
 from ingestionConfig import *
 import sys
-feedToCrawl = sys.argv[1]
+# feedToCrawl = sys.argv[1]
 # feedToCrawl = "http://feeds.feedburner.com/ndtvmovies-latest"
-ndtvFeedsObject.getUrlsFromFeed(feedToCrawl)
+feedsObject = Feeds(db, FEEDS_COLLECTION_NAME, DATA_COLLECTION_NAME)
+feedsObject.getUrlsFromFeed()
