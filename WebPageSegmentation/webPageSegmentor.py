19cb9019be0a8aacc56ea4231774d9e4356fefdc
c-shekhar <chandrashekhar.kandpal@innovaccer.com>
2017-02-17 13:40:06 +0530

    updated text density logic as per research paper

--- WebPageSegmentation/webPageSegmentor.py
+++ WebPageSegmentation/webPageSegmentor.py
@@ -1,10 +1,18 @@
 import requests
 from bs4 import BeautifulSoup,NavigableString,Tag
 from collections import OrderedDict
+from pymongo import MongoClient
+
+hostName = "localhost"
+portName = 27017
+clientObject = MongoClient(hostName,portName)
+dbName = "webDocClassification"
+dbObject = clientObject[dbName]
+coll = "Data"
 
 class BlockSegmentor():
 	def __init__(self):
-		self.restrictedTags = ["script","noscript","style"]
+		self.restrictedTags = ["script","noscript","style","meta"]
 
 	def getSoup(self, str):
 		return BeautifulSoup(str,"html.parser")
@@ -36,8 +44,7 @@ class BlockSegmentor():
 				val = self.getSoup("<" + eachBlockString.split("<")[1].strip() + "</" + blockName + ">")
 			elif ("<" not in eachBlockString.strip()):
 				val = self.getSoup(eachBlockString.strip())
-			if val not in blockDict.values():
-				blockDict[key] = val
+			blockDict[key] = val
 		# SORTING On Keys
 		orderedBlockDict = OrderedDict(sorted(blockDict.items(), key=lambda x:int(x[0].strip("B"))))
 		return orderedBlockDict
@@ -62,15 +69,21 @@ class BlockSegmentor():
 		return False
 
 	def getDoc(self, atomicBlockDict):
+		docs = []
 		for blockId, block in atomicBlockDict.iteritems():
+			metaDict = {}
 			blockWithDensity = {}
 			blockWithDensity["id"] = blockId
 			blockText = block.get_text()
 			wrappedText,tokenCount = self.getWrappedLines(blockText)
-			print blockText,"\n",self.getTextDensity(wrappedText,tokenCount)
-			# print blockText
-			# print blockText.strip().split("\n")
-	
+			blockTextDensity = self.getTextDensity(wrappedText,tokenCount)
+			# print blockId, blockText,"\n",self.getTextDensity(wrappedText,tokenCount)
+			metaDict['blockId'] = blockId
+			metaDict['data'] = blockText
+			metaDict['textDensity'] = blockTextDensity
+			docs.append(metaDict)
+		return docs
+
 	def getWrappedLines(self, textStr):    
 		wrappedTextList = []
 		tokenCount = 0
@@ -96,25 +109,45 @@ class BlockSegmentor():
 		return wrappedTextList,tokenCount
 
 	def getTextDensity(self, wrappedTextList, tokenCount):
+		tDashBx = []
 		textDensity = 0
-		if len(wrappedTextList) <= 1:
-			return textDensity
-		linesCount = len(wrappedTextList) - 1
-		textDensity = tokenCount / float(linesCount)
+		# -1 is done to exclude last line's effect
+		for i in range(len(wrappedTextList)-1):
+			tokenList = wrappedTextList[i].split()
+			tokenList = [self.stripEndChars(token) for token in tokenList]
+			tDashBx.append(tokenList)
+		flattDashBx = [item for sublist in tDashBx for item in sublist]
+		if len(wrappedTextList) > 1:
+			textDensity = len(flattDashBx)/len(wrappedTextList)-1
+		elif len(wrappedTextList) == 1:
+			singleLineTokens = wrappedTextList[0].split()
+			singleLineTokens = [self.stripEndChars(token) for token in singleLineTokens]
+			textDensity = len(singleLineTokens)
 		return textDensity
 
-if __name__ == '__main__':
-	url = "http://gadgets.ndtv.com/transportation/news/self-driving-car-prototypes-need-less-human-help-data-show-1655343"
-	# url = "http://timesofindia.indiatimes.com/business/india-business/market-moves-up-ahead-of-rbi-policy-logs-smart-weekly-gain/articleshow/56955406.cms"
-	# url = "http://timesofindia.indiatimes.com/business/india-business/coal-ministry-to-allow-commercial-mining-by-private-companies/articleshow/56940490.cms"
+	def stripEndChars(self, token):
+		return token.strip(",").strip(".").strip("-").strip(" ").strip(":")
+
+def runner(url):
 	htmlDoc = requests.get(url).text
 	soup = BeautifulSoup(htmlDoc,"html.parser")
 	blockStack = []
 	bodyTag = soup.body
 	blockSegmentorObject = BlockSegmentor()
 	blockSegmentorObject.getBlockSegments(bodyTag,blockStack)
-	blockDict = blockSegmentorObject.getAtomicBlocks(blockStack)
-	blockSegmentorObject.getDoc(blockDict)
-	# for k,v in blockDict.iteritems():
-	#     print k,v
-		# print v.get_text()
+	atomicBlockDict = blockSegmentorObject.getAtomicBlocks(blockStack)
+	return blockSegmentorObject.getDoc(atomicBlockDict)
+
+if __name__ == '__main__':
+	url1 = "http://gadgets.ndtv.com/transportation/news/self-driving-car-prototypes-need-less-human-help-data-show-1655343"
+	url2 = "http://timesofindia.indiatimes.com/business/india-business/market-moves-up-ahead-of-rbi-policy-logs-smart-weekly-gain/articleshow/56955406.cms"
+	url3 = "http://timesofindia.indiatimes.com/business/india-business/coal-ministry-to-allow-commercial-mining-by-private-companies/articleshow/56940490.cms"
+	for url in [url1,url2,url3]:
+		doc = {}
+		doc["_id"] = url
+		doc["blocks"] = runner(url)
+		try:
+			dbObject[coll].insert(doc)
+		except Exception as e:
+			print e
+		# break
