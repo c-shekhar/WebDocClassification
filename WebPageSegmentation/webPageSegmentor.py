3a1cfdc184c59eb8ac1a451b255053df39ed904e
c-shekhar <chandrashekhar.kandpal@innovaccer.com>
2017-03-02 11:28:57 +0530

    updated block fusion algorithm in segmentation

--- WebPageSegmentation/webPageSegmentor.py
+++ WebPageSegmentation/webPageSegmentor.py
@@ -13,6 +13,7 @@ coll = "Data"
 class BlockSegmentor():
 	def __init__(self):
 		self.restrictedTags = ["script","noscript","style","meta"]
+		self.threhold = 0.4
 
 	def getSoup(self, str):
 		return BeautifulSoup(str,"html.parser")
@@ -72,43 +73,43 @@ class BlockSegmentor():
 		docs = []
 		for blockId, block in atomicBlockDict.iteritems():
 			metaDict = {}
-			blockWithDensity = {}
-			blockWithDensity["id"] = blockId
 			blockText = block.get_text()
-			wrappedText,tokenCount = self.getWrappedLines(blockText)
-			blockTextDensity = self.getTextDensity(wrappedText,tokenCount)
-			# print blockId, blockText,"\n",self.getTextDensity(wrappedText,tokenCount)
+			wrappedText = self.getWrappedLines(blockText)
+			blockTextDensity = self.getTextDensity(wrappedText)
 			metaDict['blockId'] = blockId
 			metaDict['data'] = blockText
 			metaDict['textDensity'] = blockTextDensity
 			docs.append(metaDict)
-		return docs
+		fusedDocs = self.getFusedTextBlocks(docs)
+		fusedDocsWithTokenCount = self.addTokenCount(fusedDocs)
+		# print fusedDocsWithTokenCount
+		return fusedDocsWithTokenCount
 
 	def getWrappedLines(self, textStr):    
 		wrappedTextList = []
-		tokenCount = 0
 		# Assuming wmax = 80
 		# Will be managed from config in future
 		if len(textStr) > 80:
 			tokenList  = [token for token in textStr.split() if token.strip()]
-			tokenCount = len(tokenList)
 			# Avg 13 token per line
 			# Will be managed from config in future
 			# 14 = 13 + 1
-			breakPoints = [i * 14 for i in range(len(tokenList))]
+			breakPoints = [i * 13 for i in range(len(tokenList))]
 			if len(tokenList) > 13:
 				for i in breakPoints:
 					line = ""
 					line += (" ").join(tokenList[i:i+13])
 					if line:
 						wrappedTextList.append(line.encode("utf-8").decode("ascii","ignore"))
-					i += 13
+			else:
+				lineText = (" ").join([self.stripEndChars(eachToken) for eachToken in textStr.split()])
+				wrappedTextList.append(lineText)
 		else:
-			tokenCount = len(textStr.split())
-			wrappedTextList.append(textStr)
-		return wrappedTextList,tokenCount
+			lineText = (" ").join([self.stripEndChars(eachToken) for eachToken in textStr.split()])
+			wrappedTextList.append(lineText)
+		return wrappedTextList
 
-	def getTextDensity(self, wrappedTextList, tokenCount):
+	def getTextDensity(self, wrappedTextList):
 		tDashBx = []
 		textDensity = 0
 		# -1 is done to exclude last line's effect
@@ -118,12 +119,93 @@ class BlockSegmentor():
 			tDashBx.append(tokenList)
 		flattDashBx = [item for sublist in tDashBx for item in sublist]
 		if len(wrappedTextList) > 1:
-			textDensity = len(flattDashBx)/len(wrappedTextList)-1
+			textDensity = float(len(flattDashBx))/(len(wrappedTextList)-1)
 		elif len(wrappedTextList) == 1:
 			singleLineTokens = wrappedTextList[0].split()
 			singleLineTokens = [self.stripEndChars(token) for token in singleLineTokens]
-			textDensity = len(singleLineTokens)
+			textDensity = float(len(singleLineTokens))
 		return textDensity
+		
+	def getFusedTextBlocks(self, blocksList):
+		blocks = blocksList
+		nonEmptyBlocks = [eachBlock for eachBlock in blocks if eachBlock["data"]]
+		toLoop = True
+		listToIterate = nonEmptyBlocks[:]
+		tempBlock = {
+						'data' : '',
+						'textDensity' : 0,
+						'blockId' : ''
+					}
+		while toLoop:
+			toLoop = False
+			endIndex = len(listToIterate) - 1
+			i = 1
+			while (i < endIndex):
+				if (self.checkThreeBlockFusion(listToIterate[i-1],listToIterate[i],listToIterate[i+1])):
+					fusedBlock = self.fuseThreeBlocks(listToIterate[i-1],listToIterate[i],listToIterate[i+1])
+					listToIterate[i-1] = tempBlock
+					listToIterate[i] = tempBlock
+					listToIterate[i+1] = fusedBlock
+					# Skipping block i+1
+					toLoop = True 
+					i = i + 1
+				elif (self.checkTwoBlockFusion(listToIterate[i-1],listToIterate[i])):
+					fusedBlock = self.fuseTwoBlocks(listToIterate[i-1],listToIterate[i])
+					listToIterate[i-1] = tempBlock
+					listToIterate[i] = fusedBlock
+					toLoop = True
+				i = i + 1
+			listToIterate = [eachBlock for eachBlock in listToIterate if eachBlock["textDensity"] > 0]
+		return listToIterate
+	
+	def checkThreeBlockFusion(self, prevBlock, currBlock, nextBlock):
+		if (prevBlock["textDensity"] == nextBlock["textDensity"])\
+		and (currBlock["textDensity"] < prevBlock["textDensity"]):
+			return True
+		return False
+
+	def checkTwoBlockFusion(self, prevBlock, currBlock):
+		if (self.getTextDensityDelta(prevBlock,currBlock)\
+		<= self.threhold):
+			return True
+		return False
+
+	def fuseTwoBlocks(self, prevBlock, currBlock):
+		fusedBlock = {}
+		data = prevBlock["data"] + " ||" + currBlock["data"]
+		textDensity = prevBlock["textDensity"] + currBlock["textDensity"]
+		blockId = prevBlock["blockId"] + " | " + currBlock["blockId"]
+		getFusedTextBlocksock["data"] = data
+		fusedBlock["textDensity"] = textDensity
+		fusedBlock["blockId"] = blockId
+		return fusedBlock
+
+	def fuseThreeBlocks(self, prevBlock, currBlock, nextBlock):
+		fusedBlock = {}
+		data = prevBlock["data"] + " ||" + currBlock["data"] + " ||" + nextBlock["data"]
+		textDensity = prevBlock["textDensity"] + currBlock["textDensity"] + nextBlock["textDensity"]
+		blockId = prevBlock["blockId"] + " | " + currBlock["blockId"] + " | " + nextBlock["blockId"]
+		fusedBlock["data"] = data
+		fusedBlock["textDensity"] = textDensity
+		fusedBlock["blockId"] = blockId
+		return fusedBlock
+
+	def getTextDensityDelta(self, prevBlock, currBlock):
+		# return abs(prevBlock["textDensity"] - currBlock["textDensity"])
+		numrtr = abs(prevBlock["textDensity"] - currBlock["textDensity"])
+		denomtr = max(prevBlock["textDensity"], currBlock["textDensity"])
+		if denomtr == 0:
+			return 0
+		return numrtr/denomtr
+
+	def addTokenCount(self, fusedDocs):
+		fusedDocsWithTokenCount = []
+		for eachDoc in fusedDocs:
+			docWithTokenCount = eachDoc
+			data = eachDoc['data']
+			docWithTokenCount['tokenCount'] = len(data.split())
+		 	fusedDocsWithTokenCount.append(docWithTokenCount)
+		return fusedDocsWithTokenCount
 
 	def stripEndChars(self, token):
 		return token.strip(",").strip(".").strip("-").strip(" ").strip(":")
@@ -148,6 +230,7 @@ if __name__ == '__main__':
 		doc["blocks"] = runner(url)
 		try:
 			dbObject[coll].insert(doc)
+
 		except Exception as e:
 			print e
 		# break
