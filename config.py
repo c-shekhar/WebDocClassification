ab29c04bcc989350d84ab427e7a0c51c0eb79dff
mayank mittal <gmayank32@gmail.com>
2017-03-03 00:01:53 +0530

    change name from ingestionConfig to config

---
+++ config.py
@@ -0,0 +1,15 @@
+from pymongo import MongoClient
+import redis
+
+HOST_NAME = "localhost"
+PORT_NAME = 27017
+CLIENT_OBJECT = MongoClient(HOST_NAME,PORT_NAME)
+DATABASE_NAME = "webDocClassification"
+db = CLIENT_OBJECT[DATABASE_NAME]
+FEEDS_COLLECTION_NAME = "feedsInfo"
+DATA_COLLECTION_NAME = "htmlInfo"
+BLOCK_COLLECTION_NAME = "blockInfo"
+
+STATEMENT_LENGTH = 13
+MAXIMUM_CHARACTERS = 80
+
